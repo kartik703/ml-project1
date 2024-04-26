0cc0a70065f8f31e690763268cb1412b070e343f
Kartik Goswami <phoenix@Kartiks-MacBook-Air.local>
2024-04-26 09:20:11 +0530

    EDA and problem statement

--- src/exception.py
+++ src/exception.py
@@ -1,23 +1,24 @@
-import logging
 import sys
-def error_message_detail(error):
-    _, _, exc_tb = sys.exc_info()
+from src.logger import logging  # Make sure src.logger.logging exists
+
+def error_message_detail(error, error_detail):
+    _, _, exc_tb = error_detail
     file_name = exc_tb.tb_frame.f_code.co_filename
-    error_message = f"Error occurred in Python script {file_name}, line {exc_tb.tb_lineno}: {error}"
+    error_message = f"Error occurred in Python script [{file_name}] line number [{exc_tb.tb_lineno}] error message: {error}"
     return error_message
 
 class CustomException(Exception):
-    def __init__(self, error, error_detail):
-        super().__init__(error)
-        self.error_message = error_message_detail(error_detail)
+    def __init__(self, error_message, error_detail):
+        super().__init__(error_message)
+        self.error_message = error_message_detail(error_message, error_detail)
 
     def __str__(self):
         return self.error_message
 
 if __name__ == "__main__":
-    logging.basicConfig(filename='example.log', level=logging.INFO)  # Configure logging
     try:
+        # Some code that may raise an exception
         a = 1 / 0  # This will raise a ZeroDivisionError
     except Exception as e:
-        logging.info("Divide by zero error occurred")
-        raise CustomException("Divide by zero error occurred", e)
+        logging.info("An error occurred")
+        raise CustomException("An error occurred", sys.exc_info())
